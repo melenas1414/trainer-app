05dd0d4c90bbdcbaccc7ce84e00a03a5aac31f67
Santiago Galán <santiagogalan13@gmail.com>
2018-04-17 15:28:59 +0200

    Añadido funcion de numeros aletarios
    Opcion de números aleatorios indicando el numero máximo de numeros

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,14 @@
 
 var appTrainer = angular.module('appTrainer', []);
 
+
+
 appTrainer.controller('ListarColores',  ['$scope','$timeout',function ($scope, $timeout) {
     $scope.colores = ['#FFF000','#000FFF'];
     $scope.repeticion = 4;
     $scope.tiempo = 4;
     $scope.segundos = 0;
+
     $scope.formulario = 'd-block';
 	$scope.resultado = 'd-none';
     $scope.registrar = function() {
@@ -86,3 +89,79 @@ appTrainer.controller('ListarColores',  ['$scope','$timeout',function ($scope, $
 	    return false;
     }
 }]);
+appTrainer.controller('ListarNumeros',  ['$scope','$timeout',function ($scope, $timeout) {
+    
+    $scope.repeticion = 4;
+    $scope.tiempo = 4;
+    $scope.segundos = 0;
+    $scope.numero = 2;
+    $scope.formulario = 'd-block';
+	$scope.resultado = 'd-none';
+    $scope.ejecutar = function() {
+    	 if(typeof($scope.tiempo) !== 'undefined'){
+	    	 	var tiempo = $scope.tiempo * 1000 ;
+	    	 	$scope.formulario = 'd-none';
+	    	 	$scope.resultado = 'd-block';
+	    	 	$scope.segundos = 0;
+	    	 	
+	    	 	function random(max,min,rand) {
+	    	 		var numero = Math.floor(Math.random() * (max - min)) + min;
+	    	 		while(numero === rand)
+	    	 		{
+	    	 			
+	    	 			numero = Math.floor(Math.random() * (max - min)) + min;
+	    	 		}
+	  				return numero;
+				}
+				var i = 1;
+				var segundos = 0;
+				var cuentasegundos = function(){
+					if (segundos<=$scope.repeticion*$scope.tiempo) {
+			            $scope.segundos = segundos;
+			            segundos += 1;
+			            $timeout(cuentasegundos, 1000);
+			        }
+		        };
+	    	 	var cambiodefondo = function() {
+
+	    	 		if( i <= $scope.repeticion ) {
+	    	 			var rand = $scope.rand;
+	    	 			if(typeof(rand) === 'undefined') {
+	    	 				rand = 0;
+	    	 			}
+	   	 				rand=random($scope.numero,0,rand);
+
+	   	 				if ($scope.sonido){
+						    var sonido = angular.element(document.getElementById('sonido'));
+						    sonido[0].play();
+						}
+	    	 			$scope.numeros= rand;
+	    	 			$scope.rand = rand;
+	    	 			i += 1;
+			            $timeout(cambiodefondo,tiempo);
+		     		}
+		     		else{
+		     			$scope.formulario = 'd-block';
+	    	 			$scope.resultado = 'd-none';
+		     		}
+		     	}
+	     }
+	     $timeout(cambiodefondo, 0);	
+	     $timeout(cuentasegundos, 0);
+	    
+	    return false;
+    }
+}]);
+
+function nav(id) {
+	$('#sonido').stop();
+	if(id==='numeros'){
+		$('#numeros').show();
+		$('#colores').hide();
+
+	}
+	else{
+		$('#colores').show();
+		$('#numeros').hide();
+		}
+}
